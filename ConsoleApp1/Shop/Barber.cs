e96a392bef9e1fb484659d13360b6a500adaebe1
Jeppe Buthler <JeppeButhler@hotmail.com>
2021-09-17 19:21:46 +0200

    Add project files.

---
+++ ConsoleApp1/Shop/Barber.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SleepingBarber
+{
+    class Barber
+    {
+        private static int Id_s = 1;
+        private int Id = -1;
+        private ThreadState state = ThreadState.Stopped;
+        private WaitingRoom WaitingRoom;
+
+        public Barber(WaitingRoom waitingRoom)
+        {
+            Id = Id_s++;
+            this.WaitingRoom = waitingRoom;
+        }
+
+        public void GiveHaircut(Customer customer)
+        {
+            Random randomizer = new Random();
+            customer.Acquire();
+            Console.WriteLine("Barber {Id} started shaving customer {Customer.GetID}.");
+            Thread.Sleep(2000 * randomizer.Next(0, 10000));
+            customer.Shave();
+            customer.Release();
+        }
+
+        public static Boolean IsOccupied()
+        {
+            return Thread.CurrentThread.ThreadState == ThreadState.Running;
+        }
+
+        public void WakeBarber()
+        {
+            state = ThreadState.Running;
+            Console.WriteLine("The barber {Id} woke up.");
+        }
+
+        public void Sleep()
+        {
+            state = ThreadState.Stopped;
+        }
+
+        public int GetID()
+        {
+            return Id;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Shop/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SleepingBarber
+{
+    class Customer
+    {
+        private static int _id_s;
+        private int _id;
+        private Semaphore _gatekeeper;
+
+        public Customer()
+        {
+            _id = _id_s++;
+            _gatekeeper = new Semaphore(1, 1);
+            Acquire();
+        }
+
+        public void Acquire()
+        {
+            _gatekeeper.WaitOne();
+        }
+
+        public void Release()
+        {
+            _gatekeeper.Release();
+        }
+
+        public void Shave()
+        {
+            Console.WriteLine("{_id} has been shaved.");
+        }
+
+        public int GetID()
+        {
+            return _id;
+        }
+    }
+}
